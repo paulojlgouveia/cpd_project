3b6b45b69625804f5204006c7c588a9115045963
goncaloklopes@gmail.com <goncalochincho@gmail.com>
2018-03-16 14:24:35 +0000

    Table memory allocation

--- sudoku-serial.c
+++ sudoku-serial.c
@@ -1,7 +1,33 @@
 #include<stdio.h>
 #include<string.h>
+#include <stdlib.h>
+
+
+int charToInt(char c);
 
 int main(int argc, char *argv[]) {
 
+	FILE *file;
+	int l, n, lineLength;
+	int** table;
 
+
+	file = fopen(argv[1], "r");
+	
+
+	if (file) {
+		l = charToInt(getc(file));
+		n = l * l;
+		lineLength = l + (l - 1);
+
+		table = (int **)malloc(n * sizeof(int*));
+		for(int i = 0; i < l; i++){
+			table[i] = (int *)malloc(n * sizeof(int));	
+		} 
+
+	    fclose(file);
+	}
+	printf("%d",l);
 }
+
+int charToInt(char c){ return c - '0';}
