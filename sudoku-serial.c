b7fd0722b29e012060bd4356048a16c8ed166fb8
Paulo Gouveia <paulojlgouveia@gmail.com>
2018-03-23 15:03:34 +0000

    sets of cells; constructors; deleters; geters into sets; validators.

--- sudoku-serial.c
+++ sudoku-serial.c
@@ -1,3 +1,4 @@
+
 	
 /************************************************************************
  * 																		*
@@ -18,48 +19,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-// 8 spaces, 9 cells, '\0', '\n', possible '\r' on windows
+// 8 spaces, 9 cells, '\n', '\0', possible '\r' on windows
 unsigned int MAX_LINE_SIZE = 20; 
+
+/***********************************************************************************/
+
+typedef struct {
+	int x, y;
+} position;
 
 typedef struct {
 	int *candidates;
 	int x, y;
-	int checked;
-} position;
+	int value;
+} cell;
 
-typedef struct{
-	int x,y;
-} coordinate;
+// typedef struct {
+// 	cell *cells;
+// } set;
 
 
+/***********************************************************************************/
 
-int* arrayNAND(int* a1, int* a2);
 
-void printCell(position* p, int size);
+cell* EmptyCell(int candidatesSize, int x, int y);
+cell* Cell(int value, int x, int y);
+cell** Set(int size);
+cell*** Board(int size);
+
+void freeCell(cell* cell);
+void freeSet(cell** set);
+void freeBoard(cell*** board, int size);
+
+void printCell(cell* cell, int candidatesSize);
+void printSet(cell** set, int size);
+void printBoard(cell*** board, int size);
+
+int validCell(int x, int y, int bounds);
+int contains(cell** set, int value, int bounds);
+int uniqueCanditate(cell* cell, int x, int y, int candidates);
+int solved(cell*** board, int size);
+
 int charToInt(char c);
-void printBoard(position **board, int N);
+cell* getCell(cell*** board, int x, int y, int size);
+void rowToSet(cell** set, cell*** board, int x, int y, int width);
+void columnToSet(cell** set, cell*** board, int x, int y, int hight);
+void blockToSet(cell** set, cell*** board, int x, int y, int width);
 
-coordinate getBlockCoordinate(position *cellPosition, int l);
-position *getBlockTopLeft(position*** board, coordinate blockCoordinate, int l);
 
-int solved(position **board, int N);
-void checkBlockLineColumn(position*** board,int l,int n, position *pos) ;
+/***********************************************************************************/
 
-int* arrayNAND(int* a1, int* a2);
-
-void uniqueCanditate();
-void removeCandidates(int* a1, int* a2, int n);
-
-void Position(position *p, int size, int x, int y);
 
 int main(int argc, char *argv[]) {
 	
 	FILE *file;
 	int l, n;
 	int value;
-	position** board;
+	cell*** board;
+	cell** set;
 	char line[MAX_LINE_SIZE];
-	char* cell;
+	char* inputCell;
 	
 	if(argc < 2){
 		printf("missing argument.\nusage: sudoku-serial <filename>\n");
@@ -79,31 +98,23 @@ int main(int argc, char *argv[]) {
 		n = l * l;
 // 		printf("l = %d, n = %d\n", l, n);
 		
-		board = (position**) malloc(n * sizeof(position*));
-		for (int i = 0; i < n; i++) {
-			board[i] = (position*) malloc(n * sizeof(position));
-			for (int j = 0; j < n; j++) {
-				Position(&board[i][j], n, i, j);
-			}
-		}
-		
+		board = Board(n);
+		set = Set(n);
 		
 		// initialize the board
 		for (int i = 0; i < n ; i++) {
 			if (fgets(line, MAX_LINE_SIZE, file) != NULL) {
 				
-				cell = strtok(line, " ");
+				inputCell = strtok(line, " ");
 				for (int j = 0; j < n; j++) {
-					value = charToInt(*cell);
+					value = charToInt(*inputCell);
 						
 					if (value > 0)
-						board[i][j].checked = value;
-						
+						board[i][j] = Cell(value, i, j);
 					else
-						for (int t = 1; t <= n; t++)
-							board[i][j].candidates[t-1] = t;
+						board[i][j] = EmptyCell(n, i, j);
 					
-					cell = strtok(NULL, " ");
+					inputCell = strtok(NULL, " ");
 				}
 				
 			} else {
@@ -118,171 +129,215 @@ int main(int argc, char *argv[]) {
 	
 	// <FIXME> solve the thing here
 	
-	
-	
 	// </FIXME>
+		
 	
 	
-	
-
 	// display solution
 	printBoard(board, n);
 // 	char* s = (solved(board, n) != 0)? "True " : "False";
 // 	printf("%s\n", s);
 	
-	// free the memory
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			free(board[i][j].candidates);
-		}
-		free(board[i]);
-	}     
-	free(board);
+	
+	
+// 	printf("\nrow example for (1,2):\n");
+// 	rowToSet(set, board, 1, 2, n);
+// 	printSet(set, n);	
+// 	
+// 	printf("\ncolumn example for (1,2):\n");
+// 	columnToSet(set, board, 1, 2, n);
+// 	printSet(set, n);	
+// 	
+// 	printf("\nblock example for (1,2):\n");
+// 	blockToSet(set, board, 1, 2, l);
+// 	printSet(set, n);
+	
+	
+	freeBoard(board, n);
+	freeSet(set);
 	
 	return 0;
 }
 
-void Position(position *p, int size, int x, int y) {
-// 	position *p = malloc(sizeof(position));
-	p->candidates = malloc(size * sizeof(int));
-	p->x = x;
-	p->y = y;
-	p->checked = 0;
+/***********************************************************************************/
+
+cell* EmptyCell(int candidatesSize, int x, int y) {
+	cell* cell = malloc(sizeof(cell));
+	cell->x = x;
+	cell->y = y;
+	cell->candidates = malloc(candidatesSize * sizeof(int));
+	cell->value = 0;
+	
+	return cell;
 }
 
-void printBoard(position **board, int N) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			printf("%d ", board[i][j].checked);
+cell* Cell(int value, int x, int y) {
+	cell* cell = malloc(sizeof(cell));
+	cell->x = x;
+	cell->y = y;
+	cell->candidates = NULL;
+	cell->value = value;
+	
+	return cell;
+}
+
+cell** Set(int size) {
+	return malloc(size * sizeof(cell*));
+}
+
+cell*** Board(int size) {
+	cell*** board = (cell***) malloc(size * sizeof(cell**));
+	for (int i = 0; i < size; i++) 
+		board[i] = (cell**) malloc(size * sizeof(cell*));
+	
+	return board;
+}
+
+
+/***********************************************************************************/
+
+void freeCell(cell* cell) {
+	if (cell->candidates != NULL)
+		free(cell->candidates);
+	
+	free(cell);
+}
+
+void freeSet(cell** set) {
+	free(set);
+}
+
+void freeBoard(cell*** board, int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			freeCell(board[i][j]);
+		}
+		
+		free(board[i]);
+	}   
+	
+	free(board);
+}
+
+
+/***********************************************************************************/
+
+void printCell(cell* cell, int candidatesSize) {
+// 	char* checked = (p->checked == 0)? "True " : "False";
+// 	printf("(%d,%d) %s [", p->x, p->y, checked);
+	
+	printf("(%d,%d) %d [", cell->x, cell->y, cell->value);
+	
+	if (cell->candidates != NULL)
+		for (int t = 0; t < candidatesSize; t++)
+			printf(" %d", cell->candidates[t]);
+	
+	printf(" ]\n");
+}
+
+void printSet(cell** set, int size) {
+	for (int t = 0; t < size; t++)
+		printCell(set[t], size);
+	
+// 	printf("\n");
+}
+
+void printBoard(cell*** board, int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			printf("%d ", board[i][j]->value);
 		}
 		printf("\n");
 	}
 // 	printf("\n");
 }
 
-void printCell(position* p, int size) {
-// 	char* checked = (p->checked == 0)? "True " : "False";
-// 	printf("(%d,%d) %s [", p->x, p->y, checked);
+
+/***********************************************************************************/
+
+int validCell(int x, int y, int bounds) {
+	return !(x < 0 || x >= bounds || y < 0 || y >= bounds);
+}
+
+int contains(cell** set, int value, int bounds) {
+	for (int t = 0; t < bounds; t++) {
+		if (set[t]->value == value)
+			return 1;
+	}
 	
-	printf("(%d,%d) %d [", p->x, p->y, p->checked);
-	for (int t = 0; t < size; t++)
-		printf(" %d", p->candidates[t]);
+	return 0;
+}
+
+int uniqueCanditate(cell* cell, int x, int y, int candidates) {
+	int value = 0;
+	// returns on second candidate found instead of looping over all
+	for (int t = 0; t < candidates; t++) {	
+		if (cell->candidates[t] != 0) {
+			if (value != 0)
+				return 0;
+			else
+				value = cell->candidates[t];
+		}
+	}
 	
-	printf(" ]\n");
+	// if unique, update cell and return != than 0
+	cell->value = value;
+	return value;
 }
 
-int charToInt(char c) {
-	return c - '0';
-}
-
-//Returns a 0-indexed coordinate of the block
-//e.g., in a 9x9, 3x3 blocks -> (5,5) returns (1,1), (8,8) returns (2,2)
-coordinate getBlockCoordinate(position *cellPosition, int l){
-	coordinate blockCoordinate;
-
-	blockCoordinate.x = cellPosition->x / l;
-	blockCoordinate.y = cellPosition->y / l;
-
-	return blockCoordinate;
-}
-
-//returns the block's top left position
-//e.g., in a 4x4 sudoku, 2x2 blocks -> (1,0) returns (2,0)
-position *getBlockTopLeft(position*** board, coordinate blockCoordinate, int l){ 
-	return board[blockCoordinate.x * l][blockCoordinate.y * l]; 
-}
-
-int solved(position **board, int N) {
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			if (board[i][j].checked == 0)
+int solved(cell*** board, int size) {
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+			if (board[i][j]->value == 0)
 				return 0;
 			
 	return 1;
 }
 
 
-void checkBlockLineColumn(position*** board,int l,int n, position *pos) {
-	int i,numX, numY, numBlock, removables[n];
-	position *blockPos = getBlockTopLeft(board,getBlockCoordinate(pos,l),l);
+/***********************************************************************************/
 
-	//Gather candidates to remove in row and column
-	for(i = 0; i < n;i++){
-		
-		numX = board[i][pos->y]->checked;
-		numY = board[pos->x][i]->checked;
-		numBlock = board[blockPos->x + (i % n)][blockPos->y + ((int) (i/n))]->checked;
-		// check in row
-		if(numX)
-			removables[numX] = numX;
-		
-		// check in column
-		if(numY)
-			removables[numY] = numY;
-		
-		// check in block
-		if(numBlock)
-			removables[numBlock] = numBlock;
+int charToInt(char c) {
+	return c - '0';
+}
+
+cell* getCell(cell*** board, int x, int y, int size) {
+	if (validCell(x, y, size))
+		return board[x][y];
+	else
+		return NULL;
+}
+
+void rowToSet(cell** set, cell*** board, int x, int y, int width) {
+	for (int t = 0; t < width; t++)
+		set[t] = board[x][t];
+}
+
+void columnToSet(cell** set, cell*** board, int x, int y, int hight) {
+	for (int t = 0; t < hight; t++)
+		set[t] = board[t][y];
+}
+
+void blockToSet(cell** set, cell*** board, int x, int y, int width) {
+	int xStart =  width * (int) (x / width);
+	int xEnd   = xStart + width;
+	int yStart = width * (int) (y / width);
+	int yEnd   = yStart + width;
+
+	int t = 0;
+	for (int i = xStart; i < xEnd; i++) {
+		for (int j = yStart; j < yEnd; j++) {
+			set[t] = board[i][j];
+			t++;
+		}
 	}
-	
-	removeCandidates(pos->candidates, removables, n);
-}
-
-void removeCandidates(int* a1, int* a2, int n){
-	for(int i = 0; i < n; i++){
-		if(a2[i])
-			a1[i] = i+1;
-	}
-}
-
-void uniqueCanditate(position *pos, int n) {
-	int count = 0, unique;
-	for(int i  = 0; i < n; i++){
-		unique = pos->candidates[i];
-		if(unique)
-			count++;
-	}
-	if(count == 1)
-		pos->checked = unique;
-}
-//****************Might not be needed**********************
-void lineRemoval() {
-	// FIXME
-	
-	return;
 }
 
 
-void blockRemoval() {
-	// FIXME
-	
-	return;
-}
+/***********************************************************************************/
 
 
-void nakedSubset() {
-	// FIXME
-	
-	return;
-}
 
 
-void hiddenSubset() {
-	// FIXME
-	
-	return;
-}
 
 
-void xWing() {
-	// FIXME
-	
-	return;
-}
-
-
-void swordfish() {
-	// FIXME
-	
-	return;
-}
